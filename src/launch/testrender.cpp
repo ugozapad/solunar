0c4381d3bb2261fc68e7b16628527f4bef03228b
Kirill Yurkin <47507219+ugozapad@users.noreply.github.com>
2023-01-10 00:50:39 +0300

    Fix UV flip bug

--- src/launch/testrender.cpp
+++ src/launch/testrender.cpp
@@ -134,10 +134,10 @@ int main(int argc, char* argv[])
 	
 	float vertices[] = {
 		// positions           // texture coords
-		 0.5f,  0.5f, 0.0f,    1.0f, 1.0f, // top right
+		 0.5f,  0.5f, 0.0f,    1.0f, -1.0f, // top right
 		 0.5f, -0.5f, 0.0f,    1.0f, 0.0f, // bottom right
 		-0.5f, -0.5f, 0.0f,    0.0f, 0.0f, // bottom left
-		-0.5f,  0.5f, 0.0f,    0.0f, 1.0f  // top left 
+		-0.5f,  0.5f, 0.0f,    0.0f, -1.0f  // top left 
 	};
 	unsigned int indices[] = {
 		0, 1, 3, // first triangle
